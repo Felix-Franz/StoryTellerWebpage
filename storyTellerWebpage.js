66c505be3f544f8dedb9552bb68ec273116d6ec6
Felix Franz <felix-franz@t-online.de>
2017-12-17 13:06:50 +0100

    added videoplayer

--- storyTellerWebpage.js
+++ storyTellerWebpage.js
@@ -13,3 +13,15 @@ $.ajax({
 });
 var oStory = JSON.parse(sStory);
 console.log(oStory)
+
+
+function fullscreen(){
+    var payer = document.getElementById("player");
+    if (payer.requestFullscreen) {
+      payer.requestFullscreen();
+    } else if (payer.mozRequestFullScreen) {
+      payer.mozRequestFullScreen();
+    } else if (payer.webkitRequestFullscreen) {
+      payer.webkitRequestFullscreen();
+    }
+}
